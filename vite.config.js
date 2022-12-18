61158d9fbf71d2036a4ba767d3e31d0cdc5adeb8
Mathea Hansen <math21t7@stud.kea.dk>
2022-12-18 12:29:55 +0100

    tilføjet sider til dist mappe

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        vores_menu: resolve(__dirname, "src/vores-menu.html"),
+        book_os: resolve(__dirname, "src/book-os.html"),
+        koeb_gavekort: resolve(__dirname, "src/koeb-gavekort.html"),
+        kurv: resolve(__dirname, "src/kurv.html"),
+        singleview: resolve(__dirname, "src/singleview.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        om_nicecream: resolve(__dirname, "src/om-nicecream.html"),
+        find_os: resolve(__dirname, "src/find-os.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
